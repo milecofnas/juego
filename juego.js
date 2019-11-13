e59a86bfcc0035cc750f2462b54db685c376933f
milecofnas <milecofnas@gmail.com>
2019-11-13 14:51:55 +0100

    final

--- juego.js
+++ juego.js
@@ -1,51 +1,62 @@
 let jugador1 = prompt("JUGADOR 1, por favor ingrese su nombre")
 let jugador2 = prompt("JUGADOR 2, por favor ingrese su nombre")
 
-let partidasJugadas = 2
+let partidasJugadas = 3
+let puntajeJugador1 = 0
+let puntajeJugador2 = 0
 
-for (let i = partidasJugadas; partidasJugadas >= 0; i--) {
-    
-        var preguntaJugador1 = prompt(`${jugador1}: ¿Piedra, papel o tijera?`)
-        var preguntaJugador2 = prompt(`${jugador2}: ¿Piedra, papel o tijera?`)
+for (let i = partidasJugadas; puntajeJugador1 <= 2 && puntajeJugador2 <= 2; i--) {
 
-        if (preguntaJugador1 === "piedra" && preguntaJugador2 === "piedra") {
-            alert("¡Empate!")
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "papel" && preguntaJugador2 === "papel") {
-            alert("¡Empate!")
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "tijera" && preguntaJugador2 === "tijera") {
-            alert("¡Empate!")
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "piedra" && preguntaJugador2 === "tijera") {
-            var ganaJugador1 = alert(`¡Ganó ${jugador1}!`)
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "piedra" && preguntaJugador2 === "papel") {
-            var ganaJugador2 = alert(`¡Ganó ${jugador2}!`)
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "papel" && preguntaJugador2 === "tijera") {
-            var ganaJugador2 = alert(`¡Ganó ${jugador2}!`)
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "papel" && preguntaJugador2 === "piedra") {
-            var ganaJugador1 = alert(`¡Ganó ${jugador1}!`)
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "tijera" && preguntaJugador2 === "piedra") {
-            var ganaJugador2 = alert(`¡Ganó ${jugador2}!`)
-            partidasJugadas--
-        }
-        else if (preguntaJugador1 === "tijera" && preguntaJugador2 === "papel") {
-            var ganaJugador1 = alert(`¡Ganó ${jugador1}!`)
-            partidasJugadas--
-        }
-        else {
-            alert("Ingrese una respuesta válida")
-        }
+    var preguntaJugador1 = prompt(`${jugador1}: ¿piedra, papel o tijera?`)
+    var preguntaJugador2 = prompt(`${jugador2}: ¿piedra, papel o tijera?`)
+
+    if (preguntaJugador1.toLowerCase()  === "piedra" && preguntaJugador2.toLowerCase() === "piedra") {
+        alert("¡Empate!")
     }
+    else if (preguntaJugador1.toLowerCase() === "papel" && preguntaJugador2.toLowerCase() === "papel") {
+        alert("¡Empate!")
+    }
+    else if (preguntaJugador1.toLowerCase() === "tijera" && preguntaJugador2.toLowerCase() === "tijera") {
+        alert("¡Empate!")
+    }
+    else if (preguntaJugador1.toLowerCase() === "piedra" && preguntaJugador2.toLowerCase() === "tijera") {
+        alert(`¡Ganó ${jugador1}!`)
+        puntajeJugador1++
+        console.log(jugador1 + puntajeJugador1)
+    }
+    else if (preguntaJugador1.toLowerCase() === "piedra" && preguntaJugador2.toLowerCase() === "papel") {
+        alert(`¡Ganó ${jugador2}!`)
+        puntajeJugador2++
+        console.log(jugador2 + puntajeJugador2)
+    }
+    else if (preguntaJugador1.toLowerCase() === "papel" && preguntaJugador2.toLowerCase() === "tijera") {
+        (`¡Ganó ${jugador2}!`)
+        puntajeJugador2++
+        console.log(jugador2 + puntajeJugador2)
+    }
+    else if (preguntaJugador1.toLowerCase() === "papel" && preguntaJugador2.toLowerCase() === "piedra") {
+        (`¡Ganó ${jugador1}!`)
+        puntajeJugador1++
+        console.log(jugador1 + puntajeJugador1)
+    }
+    else if (preguntaJugador1.toLowerCase() === "tijera" && preguntaJugador2.toLowerCase() === "piedra") {
+        alert(`¡Ganó ${jugador2}!`)
+        puntajeJugador2++
+        console.log(jugador2 + puntajeJugador2)
+    }
+    else if (preguntaJugador1.toLowerCase() === "tijera" && preguntaJugador2.toLowerCase() === "papel") {
+        alert(`¡Ganó ${jugador1}!`)
+        puntajeJugador1++
+        console.log(jugador1 + puntajeJugador1)
+    }
+    else {
+        alert("Ingrese una respuesta válida")
+    }
+}
+if (puntajeJugador1 >= 3) {
+    alert (`¡Ganó todo el juego ${jugador1}!`)
+}
+else {
+    alert (`¡Ganó todo el juego ${jugador2}!`)
+}
 
